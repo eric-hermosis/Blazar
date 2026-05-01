b6c90d26e5aa613997ea54f7815f22cd3011e171
eric-hermosis <eric.hermosis@gmail.com>
2026-05-01 14:54:44 -0300

    Remove build folder

--- build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eric-hermosis/Escritorio/Blazar/build/_deps/googletest-src
-# Build directory: /home/eric-hermosis/Escritorio/Blazar/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
